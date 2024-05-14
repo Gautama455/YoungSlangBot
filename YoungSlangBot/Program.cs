3cc95ba2cfce4ab48115c7391e74f048b682e516
ae.dashkov <water.universe@yandex.ru>
2024-05-14 16:25:50 +0300

    Добавьте файлы проекта.

---
+++ YoungSlangBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using Telegram.Bot;
+using System;
+
+namespace YoungSlangBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
