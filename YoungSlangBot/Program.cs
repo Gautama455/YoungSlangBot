db889a380566f70830bf7aed9cecb2ec5218f52f
ae.dashkov <water.universe@yandex.ru>
2024-05-28 12:55:02 +0300

    release version 1.0

--- YoungSlangBot/Program.cs
+++ YoungSlangBot/Program.cs
@@ -1,4 +1,9 @@
-﻿using Telegram.Bot;
+﻿using System.Net.NetworkInformation;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace YoungSlangBot
 {
@@ -7,8 +12,53 @@ namespace YoungSlangBot
         static void Main(string[] args)
         {
             TelegramBotClient botClient = new TelegramBotClient(new FileReader(new FilePathEditor("BotToken.txt").GetModifiedPath()).GetContent());
-            string query = "запрос";
-            Console.WriteLine(new MessageBuilder(new HttpLinker(query)).BuildMessage());
+            //Console.WriteLine(new MessageBuilder(new HttpLinker(query)).BuildMessage());
+
+            botClient.StartReceiving(Update, Error);
+            Console.ReadLine();
+
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            Message message = update.Message;
+
+            if (message != null)
+            {
+                if (message.Text != null)
+                {
+                    if (message.Text.ToLower().Contains("здорова"))
+                    {
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Здоровей видали");
+                        return;
+                    }
+                    if (message.Text.Contains("/переведи"))
+                    {
+                        string[] messageParts = message.Text.Split(" ");
+                        if (messageParts.Length != 2)
+                        {
+                            await botClient.SendTextMessageAsync(message.Chat.Id, "Для данной команды нужен 1 параметр.");
+                            return;
+                        }
+                        else
+                        {
+                            string parametr = messageParts[1];
+                            string answerMessage = new MessageBuilder(new HttpLinker(parametr)).BuildMessage();
+                            await botClient.SendTextMessageAsync(message.Chat.Id, answerMessage);
+                            return;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }            
+        }
+
+        async static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
         }
     }
 }
